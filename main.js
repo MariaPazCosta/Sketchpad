1a9f61bbbb842d66a49d771f2e22cee13fa22a2f
MariaPazCosta <mpazcostamagna@gmail.com>
2024-08-22 21:31:21 -0300

    ya funciona changePixelColor y el boton de clear grid, ademas esta el modal pero falta mejorar el ui y hacerlo funcional

--- main.js
+++ main.js
@@ -14,4 +14,32 @@ function createGrid(numColum, numbRow){
     }
 }
 
-createGrid(10,10)
+function changePixelColor(color){
+    let pix = document.querySelectorAll(".pixel");
+    console.log(pix);
+    
+    pix.forEach((p) => {
+        p.addEventListener("mouseenter", (event) => {
+            // Cambiar el color del pixel cuando el mouse entra
+            event.target.style.backgroundColor = color; 
+        });
+    });
+    
+} 
+
+function clearGrid(){
+    let pix = document.querySelectorAll(".pixel");
+    let clearBtt = document.querySelector("#clean-grid")
+    
+    clearBtt.addEventListener("click",()=>{
+        pix.forEach(pi=>{
+            pi.style.backgroundColor="rgb(194, 194, 194)"
+        })
+    })
+}
+
+
+createGrid(15,10)
+changePixelColor("purple")
+clearGrid()
+
